1440ca6f181afd367b607dbdf6ea79feb06d1bf7
Vinicius Nobre <vina.santos06@gmail.com>
2025-04-27 15:54:11 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,12 @@ function App() {
       <section className={style.sections1} id='s1'>
         <img src={photoSlogan} alt="genkidama" className={style.img} />
         <h3 className={style.h3}>"Precisam levantar suas mãos para o céu. E assim reuniremos forças para derrotar Majin Boo. Por tudo que mais amam. Precisamos que mandem sua energia."</h3>
-        <button className={style.button} onClick={showMesssage}>Saiba mais</button>
-        {Message && (
-          <p>Genki Dama é um poderoso ataque em Dragon Ball, considerado o ataque mais poderoso da série, mas depende do número de organismos suportando seu uso. No manga, é usado apenas um total de três vezes.</p>
+        <div className={style.belowContent}>
+          <button className={style.button} onClick={showMesssage}>Saiba mais</button>
+          {Message && (
+            <p className={style.p}>Genki Dama é um poderoso ataque em Dragon Ball, considerado o ataque mais poderoso da série, mas depende do número de organismos suportando seu uso. No manga, é usado apenas um total de três vezes.</p>
           )}
+        </div>
       </section>
 
       <section className={style.sections2} id='s2'>
@@ -36,7 +38,8 @@ function App() {
           {showVideo && (
             <div className={style.video}>
             <iframe
-              width="80%"
+              className={style.sizeVideo}
+              width="50%"
               height="400"
               src="https://www.youtube.com/embed/zIwNsWjpGUU" 
               title='Goku usa a Genki Dama'
