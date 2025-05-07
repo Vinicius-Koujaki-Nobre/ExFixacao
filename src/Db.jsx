4246a3eddef5f37bff011135f27fdfb1c0fec5c8
Vinicius Nobre <vina.santos06@gmail.com>
2025-05-06 21:37:08 -0300

    Add files via upload

--- src/Db.jsx
+++ src/Db.jsx
@@ -12,14 +12,20 @@ export default function Db(){
     const [erro, setErro] = useState()
 
     useEffect(() => {
-
         api.get(`/characters?page=${page}`).then((response) => {
-            setData(response.data.items || [])
-            setErro(false)
+            const items = response.data.items || []
+    
+            if (items.length === 0) {
+                setErro(true)
+            } else {
+                setErro(false)
+            }
+    
+            setData(items);
         }).catch((error) => {
-            console.error(error)
+            console.error(error);
             setData([])
-            if(error.response && error.response.status === 404){
+            if (error.response && error.response.status === 404) {
                 setErro(true)
             }
         })
@@ -35,15 +41,25 @@ export default function Db(){
         <h5 className={style.header}><a className={style.a} href="/">VOLTAR</a></h5>
             <input className={style.input} type="text" placeholder='Digite uma página (1/6)' value={page} onChange={(e) => setPage(e.target.value)} />
 
-            {erro && <h3>Página não encontrada</h3>}
+            {erro && <h3 className={style.erro}>Página não encontrada</h3>}
 
 
             <div className={style.wrapCards}>
-                {!erro && data.length > 0 && data.map((item,index) => (
-                    <div key={index}>
-                        <Card name={item.name} image={item.image} ki={item.ki} race={item.race} gender={item.gender}/>
-                    </div>
-                ))}
+                {!erro && data.length > 0 ? (
+                    data.map((item, index) => (
+                        <div key={index}>
+                            <Card 
+                                name={item.name} 
+                                image={item.image} 
+                                ki={item.ki} 
+                                race={item.race} 
+                                gender={item.gender} 
+                            />
+                        </div>
+                    ))
+                ) : (
+                    <div style={{ height: '90vh' }}></div>
+                )}
             </div>
         </section>
         </>
